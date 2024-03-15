0c65e23454c7d1f44e30004623674f40c63fae55
Krish Behera <krishbehera10@gmail.com>
2024-03-14 22:43:10 -0400

    added button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    Get Started
-  </button>
-);
+const Button = () => {
+  const handleClick = () => {
+    window.location.href = "https://docs.google.com/document/d/142ehMW1QYaI9iWI__-AN8gHaLklV8Gl0l1Ih4bCKMs0/edit?usp=sharing";
+  };
+
+  return (
+    <button type="button" className="py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none" onClick={handleClick}>
+      Click here for the citations!
+    </button>
+  );
+};
 
 export default Button;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,17 @@
+import styles from "../style";
+import Button from "./Button";
+
 const Form = () => (
+  <>
     <section id="form" className="flex justify-center items-center h-screen ">
-      <div className = "bg-white">
+      <div className="bg-white">
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe8pjA_bm7oJK-9QEIqZF-9ScLaihwDfMgGvBZ6bFMdxZ1a1Q/viewform?embedded=true" width="640" height="787" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </div>
     </section>
-  );
-  
-  export default Form;
-  
+    <div className="flex justify-center mb-20">
+      <Button/>
+    </div>
+  </>
+);
+
+export default Form;
